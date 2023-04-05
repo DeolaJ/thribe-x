8dbcd2568793f90f675280ed65cb10def93e97fb
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-04-05 19:45:44 +0100

    update: create page sections for landing page
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from "next/head";
 
 import Header from "../src/components/Header";
+import Hero from "../src/components/Hero";
+import Footer from "../src/components/Footer";
+import About from "../src/components/About";
+import Teams from "../src/components/Teams";
+import DNA from "../src/components/DNA";
+import Contact from "../src/components/Contact";
 
 export default function Home() {
     return (
@@ -14,6 +20,12 @@ export default function Home() {
 
             <>
                 <Header bgColor="brand.gray.500" />
+                <Hero />
+                <About />
+                <Teams bgColor="brand.orange.100" />
+                <DNA />
+                <Contact bgColor="brand.blue.500" />
+                <Footer bgColor="brand.blue.500" />
             </>
         </>
     );
