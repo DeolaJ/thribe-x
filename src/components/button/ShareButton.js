e26dc5f96a8579c5e5059a3ce29f934aa5c46651
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-07-27 13:59:30 +0100

    update: create component to display links
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

---
+++ src/components/button/ShareButton.js
@@ -0,0 +1,27 @@
+import { Button } from "@chakra-ui/react";
+
+function ShareButton({ children, ...props }) {
+    return (
+        <Button
+            py="2"
+            w="100%"
+            px="4"
+            border="1.5px solid"
+            borderColor="brand.black.100"
+            bgColor="brand.orange.200"
+            boxShadow="3px 3px 0 0 var(--chakra-colors-brand-black-100)"
+            color="brand.black.100"
+            lineHeight="short"
+            fontWeight={700}
+            fontSize="md"
+            h="48px"
+            _hover={{ transform: "scale(1.03)", textDecoration: "none" }}
+            _disabled={{ cursor: "not-allowed", opacity: "0.5" }}
+            {...props}
+        >
+            {children}
+        </Button>
+    );
+}
+
+export default ShareButton;
