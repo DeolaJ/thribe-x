7274102fe45caaf5c77a0fbdc91d47e3ba0f6ac4
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-08-02 16:02:27 +0100

    feat: create form to create and update link
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- src/components/button/ShareButton.js
+++ src/components/button/ShareButton.js
@@ -14,7 +14,9 @@ function ShareButton({ children, ...props }) {
             lineHeight="short"
             fontWeight={700}
             fontSize="md"
-            h="48px"
+            whiteSpace="pre-line"
+            h="auto"
+            minH="48px"
             _hover={{ transform: "scale(1.03)", textDecoration: "none" }}
             _disabled={{ cursor: "not-allowed", opacity: "0.5" }}
             {...props}
